ebe84711fdcde95d3bcbccb5658b48c6712ffd1d
Tris Wasson <tris@paynearme.com>
2011-03-18 18:31:24 -0700

    Remove tracker from html tab titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -415,11 +415,15 @@ module ApplicationHelper
 
   def html_title(*args)
     if args.empty?
+
       title = []
       #title << @project.name if @project
       title += @html_title if @html_title
       title << Setting.app_title
       title.select {|t| !t.blank? }.join(' - ')
+      title.each {|t| t.sub!(/([a-zA-Z]+ )(#\d{1,6}:)(.?)/, '\2 \3') }
+      # ([a-zA-Z]+ )#\d{1,6}:  -- this pattern should match Bug | Feature etc followed by a 1 to 6 digit
+      # issue
     else
       @html_title ||= []
       @html_title += args
